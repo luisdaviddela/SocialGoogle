a5abc68e9b6ef18c7f3202cb4d061bb957c4c309
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-07-04 23:39:27 -0500

    Estructurads

--- GoogleClientPlugin/GoogleClientPlugin/GoogleClientPlugin/Models/GoogleUser.cs
+++ GoogleClientPlugin/GoogleClientPlugin/GoogleClientPlugin/Models/GoogleUser.cs
@@ -7,6 +7,7 @@ namespace GoogleClientPlugin
 {
     public class GoogleUser
     {
+        //--
         public string Name { get; set; }
         public string Email { get; set; }
         public Uri Picture { get; set; }
